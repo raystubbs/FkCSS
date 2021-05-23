9e5b626af1045b041fb765907c34e3d78e14cd3e
Ray Stubbs <stubbs.ray@gmail.com>
2021-05-22 20:23:05 -0600

    Fix fkcss.cljs hot reload and mount time

--- src/fkcss/cljs.cljs
+++ src/fkcss/cljs.cljs
@@ -4,17 +4,20 @@
 
 (defonce ^:private MOUNTED-STYLES (atom {}))
 
+(defn- inject-css [id config]
+  (let [node (js/document.createElement "style")]
+    (set! (.-id node) (str id))
+    (set! (.-innerHTML node) (ss/gen-css config))
+    (js/document.body.appendChild node)
+    (swap! MOUNTED-STYLES assoc id {:node node :config config})))
+
 (defn mount!
   ([]
     (mount! "fkcss-styles" {}))
   ([id config]
-    (js/window.addEventListener "DOMContentLoaded"
-      (fn []
-        (let [node (js/document.createElement "style")]
-          (set! (.-id node) id)
-          (set! (.-innerHTML node) (ss/gen-css config))
-          (js/document.body.appendChild node)
-          (swap! MOUNTED-STYLES assoc id {:node node :config config}))))))
+    (if (some? js/document.body)
+      (inject-css id config)
+      (js/window.addEventListener "DOMContentLoaded" (partial inject-css id config)))))
 
 (defn ^:private ^:dev/after-load ^:after-load re-gen []
   (doseq [{:keys [node config]} (vals @MOUNTED-STYLES)]
@@ -24,6 +27,6 @@
   ([]
     (unmount! "fkcss-styles"))
   ([id]
-    (let [el (js/document.getElementById id)]
+    (let [el (js/document.getElementById (str id))]
       (-> el .-parent (.removeChild el))
       (swap! MOUNTED-STYLES dissoc id))))
